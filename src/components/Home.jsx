9a2b600b500fcc014aeac4555dfe97b3ccef19a8
rakibhow203 <rakibhossainhow65@gmail.com>
2024-07-05 19:04:34 +0600

    Rakib's portfolio

---
+++ src/components/Home.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef } from "react";
+import pdf from "../pdf/Rakib Professional CV Resume .pdf";
+import hero from "./data/hero.json";
+import Typed from "typed.js";
+
+const Home = () => {
+  const typedRef = useRef(null);
+  useEffect(() => {
+    const options = {
+      strings: [
+        "Hi! I'm Rakib Hossain.",
+        "A Passionate Developer.",
+        "Crafting Web Solutions.",
+        "Expert in React and Node.js.",
+        "Let's Build Something Amazing Together!",
+        "Transforming Ideas into Reality!",
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    };
+
+    const typed = new Typed(typedRef.current, options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="container home" id="home">
+        <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
+          <h1 ref={typedRef}></h1>
+
+          <a
+            href={pdf}
+            download="Resume.pdf"
+            className="btn btn-outline-warning my-3"
+          >
+            Download Resume
+          </a>
+        </div>
+        <div className="right">
+          <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
+            <img src={`/assets/${hero.imgSrc}`} alt="hero" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
